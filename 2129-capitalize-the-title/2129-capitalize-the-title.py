9887447b5737c740758b6f77b9d30cbcdf003bd2
Gokulakrishnan <sgokulcse10@gmail.com>
2022-01-24 19:38:15 +0530

    Time: 32 ms (87.38%), Space: 14.1 MB (92.55%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.py
+++ 2129-capitalize-the-title/2129-capitalize-the-title.py
@@ -1,6 +1,7 @@
 class Solution:
     def capitalizeTitle(self, title: str) -> str:
         title= title.split(" ")
+        res=""
         for i in range(len(title)):
             if(len(title[i]) ==1 or len(title[i])==2):
                 title[i] = title[i].lower()
@@ -8,5 +9,6 @@ class Solution:
                 temp = title[i]
                 temp = temp[0].upper() + temp[1:].lower()
                 title[i] = temp
-        return (" ".join(title))        
+            res+=title[i]+" "
+        return res[:-1]    
         
