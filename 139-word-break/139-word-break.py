803076e0b7254c3c5212b93ecc3a1d140e21eda1
Gokulakrishnan <sgokulcse10@gmail.com>
2022-01-30 22:48:44 +0530

    Time: 62 ms (28.04%), Space: 14 MB (98.41%) - LeetHub

---
+++ 139-word-break/139-word-break.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        @lru_cache(None)
+        def helper(i):
+            if i<0:
+                return True
+            for word in  wordDict:
+                if(i >= len(word)-1 and helper(i-len(word))):
+                    if(s[i-len(word)+1 : i+1]  == word):
+                        return True
+            return False     
+        return helper(len(s)-1)
+                    
+        
