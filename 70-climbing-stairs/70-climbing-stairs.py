10876b9e17f0704d865b22d4fc3327a84d6b975c
Gokulakrishnan <sgokulcse10@gmail.com>
2022-01-31 22:57:33 +0530

    Time: 24 ms (96.66%), Space: 13.9 MB (94.38%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        @lru_cache(None)
+        def solve(step):
+            if step==n:
+                return 1
+            if step>n:
+                return 0
+            return solve(step+1) + solve(step+2)
+        return solve(0)
+
+            
+        
