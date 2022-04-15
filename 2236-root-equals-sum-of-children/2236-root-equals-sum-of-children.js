1038ccf58894b68add84c4f10fc3c47ed945a5f4
Gokulakrishnan <sgokulcse10@gmail.com>
2022-04-15 23:04:37 +0530

    Time: 66 ms (74.77%), Space: 42.7 MB (91.59%) - LeetHub

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var checkTree = function(root) {
+    if(root.right.val + root.left.val === root.val){
+        return true
+    }
+    return false
+};
