585c01ff025307088b89e152c4543500fe097495
Gokulakrishnan <sgokulcse10@gmail.com>
2022-04-16 20:39:39 +0530

    Time: 88 ms (83.72%), Space: 16.8 MB (53.49%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
@@ -8,9 +8,21 @@ class Solution:
     def __init__(self):
         self.total = 0
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if(root is not None):
-            self.convertBST(root.right)
-            self.total += root.val
-            root.val = self.total
-            self.convertBST(root.left)
+        # if(root is not None):
+        #     self.convertBST(root.right)
+        #     self.total += root.val
+        #     root.val = self.total
+        #     self.convertBST(root.left)
+        # return root    
+        stack = []
+        node = root
+        while(node or stack):
+            while(node is not None):
+                stack.append(node)
+                node = node.right
+            node = stack.pop()
+            self.total += node.val
+            node.val = self.total
+            node = node.left
         return root    
+        
