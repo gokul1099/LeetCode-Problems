6a0c4781e6d27261331aed914ae7431c89a5143f
Gokulakrishnan <sgokulcse10@gmail.com>
2022-05-11 22:02:12 +0530

    Time: 454 ms (38.09%), Space: 16.5 MB (62.74%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(i,j):
-            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]!="1":
+        def recur(i,j):
+            if(i<0 or i>=row or j<0 or j>=col or grid[i][j]!="1"):
                 return
-            grid[i][j]="-1"
-            bfs(i,j-1)
-            bfs(i,j+1)
-            bfs(i-1,j)
-            bfs(i+1,j)
-        count=0    
-        if len(grid)==0:
-            return 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if(grid[i][j]=="1"):
-                    count+=1
-                    bfs(i,j)
-        return count          
+            grid[i][j] = "-1"
+            recur(i,j-1)
+            recur(i,j+1)
+            recur(i+1,j)
+            recur(i-1,j)
             
-                
-        
+        row = len(grid)
+        col = len(grid[0])
+        if(len(grid)==0):
+            return 0
+        count=0
+        for i in range(row):
+            for j in range(col):
+                if(grid[i][j] == "1"):
+                    count+=1
+                    recur(i,j)        
+        return count            
